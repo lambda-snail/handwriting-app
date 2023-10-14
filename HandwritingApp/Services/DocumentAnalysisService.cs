f2006bdf56c9467513f0f65783129b9cceeb4384
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-10-14 16:32:05 +0200

    add document intelligence integration

---
+++ HandwritingApp/Services/DocumentAnalysisService.cs
@@ -0,0 +1,28 @@
+﻿using Azure;
+using Azure.AI.FormRecognizer.DocumentAnalysis;
+using Azure.Identity;
+
+namespace HandwritingApp.Services;
+
+public class DocumentAnalysisService
+{
+    private readonly DocumentAnalysisClient _client;
+    
+    public DocumentAnalysisService(DefaultAzureCredential credential)
+    {
+        string endpoint = "https://westeurope.api.cognitive.microsoft.com/";
+        _client = new DocumentAnalysisClient(new Uri(endpoint), credential);
+    }
+
+    public async Task<AnalyzeResult> AnalyzeHandwriting(Uri uri)
+    {
+        AnalyzeDocumentOperation operation = await _client.AnalyzeDocumentFromUriAsync(WaitUntil.Completed, "prebuilt-layout", uri);
+        return operation.Value;
+    }
+    
+    public async Task<AnalyzeResult> AnalyzeHandwriting(Stream stream)
+    {
+        AnalyzeDocumentOperation operation = await _client.AnalyzeDocumentAsync(WaitUntil.Completed, "prebuilt-layout", stream);
+        return operation.Value;
+    }
+}
