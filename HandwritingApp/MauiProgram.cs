1bf7d383d5700a5b54cb35cd29f8a5eda9e16d8c
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-10-14 22:05:13 +0200

    remove some unused code

--- HandwritingApp/MauiProgram.cs
+++ HandwritingApp/MauiProgram.cs
@@ -1,9 +1,7 @@
 ﻿using Azure.Identity;
 using Azure.Storage.Blobs;
 using Microsoft.Extensions.Logging;
-using HandwritingApp.Data;
 using HandwritingApp.Services;
-using Microsoft.AspNetCore.Authorization;
 using MudBlazor.Services;
 
 namespace HandwritingApp;
@@ -25,11 +23,7 @@ public static class MauiProgram
 #endif
 
         builder.Services.AddMudServices();
-        
-        builder.Services.AddSingleton<WeatherForecastService>();
-        
-        builder.Services.AddSingleton<DefaultAzureCredential>(new DefaultAzureCredential());
-        
+
         builder.Services.AddScoped<BlobServiceClient>(provider =>
         {
             var credential = provider.GetRequiredService<DefaultAzureCredential>();
