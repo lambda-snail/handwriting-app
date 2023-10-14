d5eefd382c22174e03ac1a1d4e70d807611b64c7
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-10-14 23:56:25 +0200

    add connection string to blob storage

---
+++ HandwritingApp/Services/BlobClientFactory.cs
@@ -0,0 +1,22 @@
+﻿using Azure.Storage.Blobs;
+using OneOf;
+
+namespace HandwritingApp.Services;
+
+public record MissingConnectionString();
+
+public class BlobClientFactory
+{
+    public async Task<OneOf<BlobServiceClient, MissingConnectionString>> GetBlobClientAsync()
+    {
+        string connectionString = await SecureStorage.Default.GetAsync("blob_connection_string");
+        if (!string.IsNullOrWhiteSpace(connectionString))
+        {
+            return new BlobServiceClient(connectionString);
+        }
+        else
+        {
+            return new MissingConnectionString();
+        }
+    }
+}
